8b516460fde876ec1682851398c6bb46079efda7
Patrick Q <pquach3@my.bcit.ca>
2023-03-08 13:47:22 -0800

    fixed the prompt to properlly loop if invalid data is inputed

--- script/script.js
+++ script/script.js
@@ -1,10 +1,9 @@
 const couseList = [{code: "ACIT 1630", name: "Database Systems"}, {code: "ACIT 1420", name: "Intro to Systems Admin"}, {code: "ORGB 1100", name: "Organizational Behaviour"}];
-let valid = true;
-while (valid) {
-    let course = prompt("Enter a 4-digit course code: ");
-    if (toString(course).legnth == 5) or (typeof(course) == "number")
-    {
-        valid = false;
-    }
-}
+
+do {
+    courseCode = prompt("Enter a 4-digit course code: ");
+    console.log(courseCode);
+    console.log(typeof(courseCode));
+    if ((courseCode >= 0) && (courseCode <= 9999)) { break;}
+} while (true)
 
