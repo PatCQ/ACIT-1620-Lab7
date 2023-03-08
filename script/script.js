557e8ba3dbfc9a33fc79f7f7e61cfa3c4cb6c5b1
Patrick Q <pquach3@my.bcit.ca>
2023-03-08 14:00:09 -0800

    Checked inputed code with codes listed in the array and outputs a answer if found

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
-const couseList = [{code: "ACIT 1630", name: "Database Systems"}, {code: "ACIT 1420", name: "Intro to Systems Admin"}, {code: "ORGB 1100", name: "Organizational Behaviour"}];
+const courseList = [{code: "ACIT 1630", name: "Database Systems"}, {code: "ACIT 1420", name: "Intro to Systems Admin"}, {code: "ORGB 1100", name: "Organizational Behaviour"}];
 
 do {
     courseCode = prompt("Enter a 4-digit course code: ");
-    console.log(courseCode);
-    console.log(typeof(courseCode));
     if ((courseCode >= 0) && (courseCode <= 9999)) { break;}
 } while (true)
 
+for (let course of courseList) {
+    str = course['code'].slice(5)
+    if (courseCode == str) {
+        console.log("Yes I am taking the course: " + course['code'] + " - " + course['name']);
+        break;
+    }
+    else {
+        console.log("No")
+    }
+}
